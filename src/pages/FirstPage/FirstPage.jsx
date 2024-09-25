0cb7e39f6f80926a9612ada0e16b1c76499a70c0
Denys Maksymchuk <denysmaksimchuk@gmail.com>
2024-09-25 19:14:04 +0200

    new brench

--- src/pages/FirstPage/FirstPage.jsx
+++ src/pages/FirstPage/FirstPage.jsx
@@ -1,11 +1,11 @@
-import { Container, Title, StyledImage } from './FirstPage.styled';
-import example from '../../assets/example.png';
+import { Container, Title } from './FirstPage.styled';
+// import example from '../../assets/example.png';
 
 const FirstPage = () => {
   return (
     <Container>
       <Title>First Page</Title>
-      <StyledImage src={example} alt="Example" />
+      {/* <StyledImage src={example} alt="Example" /> */}
     </Container>
   );
 };
